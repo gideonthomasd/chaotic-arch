08f56c2a9731bc96ecfc7512664877ecadf1c728
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-25 11:21:10 +0100

    Add files via upload

--- 125_yay.sh
+++ 125_yay.sh
@@ -4,4 +4,5 @@ paru -S nerd-fonts-mononoki
 paru -S nerd-fonts-jetbrains-mono
 paru -S brother-brgenml1
 paru -S arcolinux-logout
+sudo pacman -S cups gutenprint cups-pdf gtk3-print-backends nmap
 #paru -S steam-fonts
