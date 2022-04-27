c7620605cb78d4abc6b778fa83c016616b7c1faf
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-27 08:16:01 +0100

    Update 125_yay.sh

--- 125_yay.sh
+++ 125_yay.sh
@@ -5,4 +5,5 @@ paru -S nerd-fonts-jetbrains-mono
 paru -S brother-brgenml1
 paru -S arcolinux-logout
 sudo pacman -S cups gutenprint cups-pdf gtk3-print-backends nmap
+paru -S brother-mfc-j4510dw
 #paru -S steam-fonts
