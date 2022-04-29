d36e30776c3d942a5c24c0db77537c2af38f53a8
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-29 01:07:04 +0100

    Update 220-Nvidia.sh

--- 220-Nvidia.sh
+++ 220-Nvidia.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S nvidia nvidia-lts
-sudo pacman -S lib32-nvidia-utils
+sudo pacman -S 	nvidia-470xx-dkms nvidia-470xx-utils lib32-nvidia-470xx-utils
 sudo pacman -S steam
+
+#sudo pacman -S nvidia nvidia-lts
+#sudo pacman -S lib32-nvidia-utils
+#sudo pacman -S steam
