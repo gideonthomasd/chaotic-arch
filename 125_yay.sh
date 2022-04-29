c16d1e806f0e962d5b69bfa9c91bfab332ec7073
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-29 14:39:19 +0100

    Update 125_yay.sh

--- 125_yay.sh
+++ 125_yay.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-paru -S nerd-fonts-mononoki
-paru -S nerd-fonts-jetbrains-mono
-paru -S brother-brgenml1
-paru -S arcolinux-logout
+yay -S nerd-fonts-mononoki
+yay -S nerd-fonts-jetbrains-mono
+yay -S brother-brgenml1
+yay -S arcolinux-logout
 sudo pacman -S cups gutenprint cups-pdf gtk3-print-backends nmap
-paru -S brother-mfc-j4510dw
+yay -S brother-mfc-j4510dw
 #paru -S steam-fonts
